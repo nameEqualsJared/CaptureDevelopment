26f1d32c9cce0606b515fb18dab92f0cc0ba2945
Jared H <harp.62@osu.edu>
2018-12-17 15:43:02 -0500

    Base, working commit

---
+++ popup.js
@@ -0,0 +1,144 @@
+
+
+//linking up all the buttons
+let saveButton = document.getElementById("saveButton");
+let openSnipsButton = document.getElementById("openSnipsButton");
+
+//initializing the DBs (will either create a new one or link back up to the currently existing one)
+let dbForSnips = new PouchDB("dbForSnips");
+let dbForTags = new PouchDB("dbForTags");
+
+//defining a new object (providing constructor function for it)
+// Snip -- the object used to store a singular snip. 
+function Snip(_id, url, title, snipText, tags) {
+	this._id = _id;  //A string with the date of when the snip was created. Also the unique ID used to save the snip in PouchDB 
+	this.url = url;
+	this.title = title;
+	this.snipText = snipText;
+	this.tags = tags; //an array of all the tags contained in the snipText
+}
+//maybe refactor out the constructor; I don't really need it, could just use object literal syntax
+
+
+function saveANewSnip() {
+	//NOTE: this function modified a global variable! Specifically, it will put the id of the snip that it saves in the "idOfSnipIfAlreadySaved" global!
+
+
+	//get the current page title and address
+	chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+		let currentURL = tabs[0].url;
+		let currentTitle = tabs[0].title;
+
+		//"tags" will hold all the tags in the snipText (without hashtags)
+		let tags = [];
+		let tagArrayWithHashtags = snipText.match(/(#[1-9a-zA-z-]+)/g);
+		if (tagArrayWithHashtags) {
+			//if the tagArrayWithHashtags exists (not null)
+			for (let tag of tagArrayWithHashtags) {
+				tags.push(tag.slice(1));
+				//simply add the tags to the array, with the hashtags removed
+			}
+		}
+
+		//_id holds the date of when this snip was created in a string 
+		_id = new Date().toISOString();
+
+		//Constructing the new snip.
+		currentSnip = new Snip(_id, currentURL, currentTitle, snipText, tags);
+
+
+		//saving this snip to all of its tags in dbForTags
+		for (let tag of currentSnip.tags) {
+			dbForTags.get(tag, function (err, doc) {
+				if (err) {
+					if (err.message === "missing") {
+						//this error means the document doesn't exist. 
+						//thus, we create it
+						dbForTags.put({ _id: tag, snipsWithThisTag: [currentSnip._id] });
+
+					} else {
+						//otherwise, just log the error 
+						console.log(err)
+					}
+
+				} else {
+					// if we reach here, the document exists
+					//thus, we get the snipsWithThisTag array out, add on the current snip, and save it back 
+					let snipsWithThisTag = doc.snipsWithThisTag;
+					snipsWithThisTag.push(currentSnip._id);
+					dbForTags.put(doc);
+				}
+
+			});
+		}
+
+		//saving the Snip in the dbForSnips
+		dbForSnips.put(currentSnip, function callback(err, result) {
+			if (err) {
+				console.log(err);
+			}
+		});
+
+		//push the _id of the currently saved snip into the global
+		idOfSnipIfAlreadySaved.push(_id);
+
+		console.log(currentSnip);
+
+	});
+
+	//get the current snip text given by the user
+	let snipText = document.getElementById("inputText").value;
+
+	//Remember that we execute ALL the sync stuff first, and then the async stuff!
+	//Thus, the order of execution here is "30" (the function is kicked off but remember it is async!), then 92 (so getting the snipText), then all the stuff inside 30 when it finishes and "calls us back" :) (at which point the snipText will be available!!! That's the key to realize: really the order of execution is more like 90 then when the function calls us back :))
+
+
+}
+
+
+idOfSnipIfAlreadySaved = [];
+//meant to be a global. Will only ever hold one element; the id of the snip if one has been saved.
+
+saveButton.onclick = function () {
+
+	if (idOfSnipIfAlreadySaved.length === 0) {
+		//no snips have been saved yet. (ie, this is the first time the save button has been pushed) 
+
+		saveANewSnip();
+
+	} else {
+		//One snip has been saved so far.
+
+		let idOfOldSnip = idOfSnipIfAlreadySaved[0];
+
+		//check if the snipText has changed (don't want to do anything if it hasn't)
+		dbForSnips.get(idOfOldSnip, function (err, doc) {
+			if (err) {
+				console.log(err);
+			} else {
+				if (doc.snipText !== document.getElementById("inputText").value) {
+					//the snipText has changed.
+
+					//delete the old snip 
+					deleteSnip(doc);
+
+					//clear this out; we'll be saving a new snip.
+					idOfSnipIfAlreadySaved.pop();
+
+					saveANewSnip();
+
+				}
+			}
+
+		});
+	}
+
+
+};
+
+
+openSnipsButton.onclick = function () {
+
+	chrome.tabs.create({ url: "snips.html" });
+
+}
---
+++ snipModule.js
@@ -0,0 +1,46 @@
+//this module provides a common function to popup.js and snips.js
+
+
+function deleteSnip(snip) {
+	//snip -- the document to be removed (the whole thing; not just the id);
+
+	//this function properly deletes a snip: that is, not only does it remove it from the dbForSnips, but it also clears its id out of any tags in dbForTags. 
+
+	let dbForSnips = new PouchDB("dbForSnips");
+	let dbForTags = new PouchDB("dbForTags");
+
+	//For each tag in this snip, go into the dbForTags, and get out the snipsWithThisTag array for each tag. Remove this snip's id; we are deleting it.
+	for (let tag of snip.tags) {
+		dbForTags.get(tag, function (err, doc) {
+			if (err) {
+				console.log(err);
+			} else {
+
+				let snipsWithThisTag = doc.snipsWithThisTag;
+
+				//remove the id from the snipsWithThisTag array (cause we are deleting the snip)
+				let index = snipsWithThisTag.indexOf(snip._id);
+				if (index > -1) {
+					snipsWithThisTag.splice(index);
+				}
+
+				if (snipsWithThisTag.length === 0) {
+					//no more snips exist with this tag. So just delete the whole entry in dbForTags.
+
+					dbForTags.remove(doc);
+
+				} else {
+					//otherwise update the entry
+					dbForTags.put(doc);
+
+				}
+
+			}
+
+		});
+	}
+
+	//delete this snip from dbForSnips
+	dbForSnips.remove(snip, function (err) { if (err) { console.log(err); } });
+
+}
